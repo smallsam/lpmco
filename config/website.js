9178dec43f5ab32455c087f6328d55e6d37053a4
Sam Richards <srichards@cloudbolt.io>
2020-03-13 15:13:25 +1000

    Updated logos for og

--- config/website.js
+++ config/website.js
@@ -8,7 +8,7 @@ module.exports = {
     siteTitleShort: 'lpmco', // short_name for manifest
     siteHeadline: 'managing projects for social good', // Headline for schema.org JSONLD
     siteUrl: 'https://www.lpmcompany.com.au', // Domain of your site. No trailing slash!
-    siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
+    siteLogo: '/logos/lpm-logo-1024.png', // Used for SEO and manifest
     siteLanguage: 'en', // Language Tag on <html> element
     siteDescription: 'LPMCo, managing projects for social good',
     author: 'lpmco', // Author for schema.org JSONLD
